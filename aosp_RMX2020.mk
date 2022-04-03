764569fd960191794dae0e3f2f558f3253d74afa
Sarthak Roy <sarthakroy2002@gmail.com>
2022-04-03 10:01:52 +0530

    RMX2020: PixelOS bringup
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I001ee3f90e50a292467b5a0aabe42b94cbd34973

--- aosp_RMX2020.mk
+++ aosp_RMX2020.mk
@@ -11,20 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_RMX2020
+PRODUCT_NAME := aosp_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme Monet
 PRODUCT_MANUFACTURER := realme
 
 TARGET_BOOT_ANIMATION_RES := 720
-
-# ArrowOS additions
-DEVICE_MAINTAINER := sarthakroy2002
 
 # Build info
 BUILD_FINGERPRINT := "google/coral/coral:12/SP2A.220305.012/8177914:user/release-keys"
