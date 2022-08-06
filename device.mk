14d1136b03a63bf01414719bd537ad5556a81bee
bengris32 <bengris32@protonmail.ch>
2022-08-06 20:49:58 +0100

    RMX2020: vibrator: Add basic Vibrator AIDL service
    * Very barebones implementation that just uses LED vibrator
      trigger.
    
    * Based on the QTI vibrator service, with unrelated haptic
      cruft removed.
    
    * Also drop the prebuilt MTK vibrator service.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I7935dd85987464aa529f2779e154ca611cfcf532

--- device.mk
+++ device.mk
@@ -405,7 +405,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-V2-ndk_platform.vendor
+    android.hardware.vibrator-service.RMX2020
 
 # VNDK
 PRODUCT_COPY_FILES += \
