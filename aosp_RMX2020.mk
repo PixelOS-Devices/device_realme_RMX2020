77bf3bd95b92f2c728a6b5927ba6bb616ccb6e7f
Sarthak Roy <sarthak@pixelos.net>
2023-01-09 19:07:20 +0530

    RMX2020: Switched to common_full_phone
    Signed-off-by: Sarthak Roy <sarthak@pixelos.net>
    Change-Id: Iaae4499f0ff9a953db6f749b1259c710d4c18baf

--- aosp_RMX2020.mk
+++ aosp_RMX2020.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_RMX2020
