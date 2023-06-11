45421754b0ee5db66d19493f0be9aa638b2aded7
Sarthak Roy <sarthakroy2002@gmail.com>
2023-06-11 11:52:51 +0530

    RMX2020: Drop MIUI charger impl
    Change-Id: I03d178d50a5119325d7db50eab93986889794190
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -292,11 +292,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.android.nfc_extras.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Pixel Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor
